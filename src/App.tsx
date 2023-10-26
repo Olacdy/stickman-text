d86a316d4c1907cde58ba8b1730ee074ebe539b8
Олег Дидечкин <crm.29431@dz.com>
2023-10-26 16:22:53 +0300

    Hint on how to copy stickman

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ const App = () => {
       <main className='relative flex items-center justify-center h-screen max-h-screen overflow-hidden'>
         <ThemeToggle className='absolute right-5 top-5' />
         <StickmanText />
+        <p className='absolute text-sm -translate-x-1/2 bottom-3 left-1/2 text-muted-foreground/60 dark:text-muted-foreground/30'>
+          *click on stickman to copy text
+        </p>
       </main>
       <Toaster
         richColors
